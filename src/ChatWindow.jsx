c3a66308e47bf100537013b951e53d363c3ea4ac
Sakshi Sahu <sahusaksho889@gmail.com>
2025-08-17 18:20:43 +0530

    Fix import path casing

--- src/ChatWindow.jsx
+++ src/ChatWindow.jsx
@@ -1,6 +1,7 @@
 
 import "./ChatWindow.css";
-import Chat from "./chat.jsx";
+import Chat from "./Chat.jsx";
+
 import { MyContext } from "./MyContext.jsx";
 import { useContext, useState, useEffect } from "react";
 import {ScaleLoader} from "react-spinners";
